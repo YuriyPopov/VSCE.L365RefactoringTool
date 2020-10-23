031d2304f9275671cde0e1dd3808aeb66cbff3b7
Yuriy Popov <yp.fastbox@gmail.com>
2020-10-23 14:24:55 +0300

    Make separate tooltip commands for workspace and curr. doc.

--- extension.js
+++ extension.js
@@ -1,46 +1,73 @@
 const vscode = require('vscode');
 const fs = require('fs');
 
+function processDocTxt(docTxt) {
+
+	let modified = false;
+
+	let txt = docTxt.replace(/\baction\b\s*\(\s*(("(("")|[^\"])*")|[_0-9a-zA-Z]\w*)\s*\)[\n\s]*{(([^}]*)\n*)*?}/gmi, fragment => {
+
+		let indent = fragment.match(/^\s*(?=AccessByPermission|ApplicationArea|Caption|CaptionML|Description|Ellipsis|Enabled|Gesture|Image|InFooterBar|ObsoleteReason|ObsoleteState|ObsoleteTag|Promoted|PromotedCategory|PromotedIsBig|PromotedOnly|RunObject|RunPageLink|RunPageMode|RunPageOnRec|RunPageView|Scope|ShortcutKey|ToolTip|ToolTipML|Visible)/mi);
+
+		let captions = [fragment.match(/(?<=\baction\b\s*\(\s*)(("(("")|[^"])*")|[_0-9a-zA-Z]\w*)\s*/), fragment.match(/(?<=\bCaption\s*=\s*')[^']+/)];
+		let tooltip = [''];
+
+		captions.forEach((item) => {
+			if (item) tooltip.unshift(item[0]);
+		});
+
+		return fragment.replace(/(?:[\s\n]*\b(?:AccessByPermission|ApplicationArea|Caption|CaptionML|Description|Ellipsis|Enabled|Gesture|Image|InFooterBar|ObsoleteReason|ObsoleteState|ObsoleteTag|Promoted|PromotedCategory|PromotedIsBig|PromotedOnly|RunObject|RunPageLink|RunPageMode|RunPageOnRec|RunPageView|Scope|ShortcutKey|ToolTip|ToolTipML|Visible)\b[^;]+;(?:\s*\/\/.*)?)+/mi, fragment => {
+			modified = !fragment.match(/\bToolTip[^;]+;/gmi);
+			return ((modified) ? `${fragment}${((indent) ? indent[0] : '\t\t\t\t')}ToolTip = '${tooltip[0]}'; //TODO: Write tooltip` : fragment)
+		});
+	});
+
+	return { txt: txt, modified: modified };
+}
+
+
 function activate(context) {
 
 	let output = vscode.window.createOutputChannel('L365 Refactoring')
-	let disposable = vscode.commands.registerCommand('refactoring-tool-l365.action-tooltip', function () {
+
+	context.subscriptions.push(vscode.commands.registerCommand('refactoring-tool-l365.set-action-tooltip-doc', function () {
+
+		let editor = vscode.window.activeTextEditor;
+
+		if (editor) {
+
+			let selection = editor.selection;
+			let text = ((selection.isEmpty) ? editor.document.getText() : editor.document.getText(selection).trim());
+			let result = processDocTxt(text);
+
+			if (result.modified) {
+				editor.edit(editBuilder => {
+					editBuilder.replace((((selection.isEmpty) ? new vscode.Range(editor.document.lineAt(0).range.start, editor.document.lineAt(editor.document.lineCount - 1).range.end) : selection)), result.txt);
+				});
+			}
+		}
+
+	}));
+
+	context.subscriptions.push(vscode.commands.registerCommand('refactoring-tool-l365.set-action-tooltip-worksp', function () {
 
 		output.clear();
 		output.show();
 		output.appendLine('Analizing workspace...');
-		
+
 		vscode.workspace.findFiles('**/*.al').then(files => {
 			files.forEach(uri => {
 				vscode.workspace.openTextDocument(uri).then(doc => {
-					
-					let modified = false;
-					let docTxt = doc.getText()
-					.replace(/\baction\b\s*\(\s*(("(("")|[^\"])*")|[_0-9a-zA-Z]\w*)\s*\)[\n\s]*{(([^}]*)\n*)*?}/gmi, fragment => {
-						
-						let indent = fragment.match(/^\s*(?=AccessByPermission|ApplicationArea|Caption|CaptionML|Description|Ellipsis|Enabled|Gesture|Image|InFooterBar|ObsoleteReason|ObsoleteState|ObsoleteTag|Promoted|PromotedCategory|PromotedIsBig|PromotedOnly|RunObject|RunPageLink|RunPageMode|RunPageOnRec|RunPageView|Scope|ShortcutKey|ToolTip|ToolTipML|Visible)/mi);
-						
-						let captions = [fragment.match(/(?<=\baction\b\s*\(\s*)(("(("")|[^"])*")|[_0-9a-zA-Z]\w*)\s*/), fragment.match(/(?<=\bCaption\s*=\s*')[^']+/)];
-						let tooltip = [''];
-						
-						captions.forEach((item) => {
-							if (item) tooltip.unshift(item[0]);
-						});
-						
-						return fragment.replace(/(?:[\s\n]*\b(?:AccessByPermission|ApplicationArea|Caption|CaptionML|Description|Ellipsis|Enabled|Gesture|Image|InFooterBar|ObsoleteReason|ObsoleteState|ObsoleteTag|Promoted|PromotedCategory|PromotedIsBig|PromotedOnly|RunObject|RunPageLink|RunPageMode|RunPageOnRec|RunPageView|Scope|ShortcutKey|ToolTip|ToolTipML|Visible)\b[^;]+;(?:\s*\/\/.*)?)+/mi, fragment => {
-							modified = !fragment.match(/\bToolTip[^;]+;/gmi);
-							return ((modified) ? `${fragment}${((indent) ? indent[0] : '\t\t\t\t')}ToolTip = '${tooltip[0]}'; //TODO: Write tooltip` : fragment)
-						});
-					});
-					
-					if (modified) fs.writeFileSync(uri.fsPath, docTxt);
+
+					let result = processDocTxt(doc.getText());
+
+					if (result.modified) fs.writeFileSync(uri.fsPath, result.txt);
 				})
 			});
 		});
-	});
-
-	context.subscriptions.push(disposable);
+	}));
 }
+
 exports.activate = activate;
 
 function deactivate() { }
